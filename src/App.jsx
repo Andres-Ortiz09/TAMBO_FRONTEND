235ebdf6a95c89c5f272b1652e35c5892a4c5a13
Leo <leonsoles6@hotmail.com>
2025-09-18 01:18:26 -0500

    Agregue la ruta de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Registro from "./Components/Registro"; 
+import Login from "./Components/Login";
 
 function App() {
   return (
     <div className="App">
-      <Registro />
+      <Login />
     </div>
   );
 }
