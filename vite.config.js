9ddfb7aa2c8b022bbbc2884470648576fee482bf
AnghelloVT <u22325555@utp.edu.pe>
2025-10-28 23:45:01 -0500

    Vite config corregido

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // permite conexiones externas en desarrollo local 
+  },
+  preview: {
+    host: '0.0.0.0', // exponer el puerto
+    port: process.env.PORT || 4173, // usa el puerto que se asigne
+    allowedHosts: ['tambo-frontend.onrender.com'], //dominio
+  }
 })
