8de35ed947618d0f2db64290b683726f993fc9b4
AnghelloVT <u22325555@utp.edu.pe>
2025-11-18 12:45:10 -0500

    Update clients.js

--- src/clients.js
+++ src/clients.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://localhost:8080/api", 
+  baseURL: "https://tambo-backend.onrender.com/api", 
   headers: {
     "Content-Type": "application/json",
   },
